ed901eb25c45e0dce1f2490cca0cade0986cdc6a
BOBAH1988 <79042044926@ya.ru>
2023-02-28 11:07:06 +0300

    finish dz

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
 // 782 -> 8
 // 918 -> 1
 
-
+// Console.WriteLine("Введите 3х значное число: ");
+// int value = Convert.ToInt32(Console.ReadLine());
+// if(value > 99 && value < 1000)
+// {
+//     int number = (value / 10) % 10;
+//     Console.WriteLine($"Ответ: {number}");
+// }
+// else
+// {
+//     Console.WriteLine($"Вы ввели не 3х значное чило!");
+// }
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру ПЯТИЗНАЧНОГО числа 
@@ -13,7 +23,17 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-
+// Console.WriteLine("Введите 5-ти значное число: ");
+// int value = Convert.ToInt32(Console.ReadLine());
+// if(value > 9999 && value < 100000)
+// {
+//     int number = (value / 100) % 10;
+//     Console.WriteLine($"Ответ 1: {number}");
+// }
+// else
+// {
+//     Console.WriteLine($"Третей цифры нет!");
+// }
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, 
@@ -22,3 +42,18 @@
 // 7 -> да
 // 1 -> нет
 
+// Console.WriteLine("Введите день недели, цифрой: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if(day > 0 && day < 8)
+// {
+//     if(day > 5)
+//     {
+//         Console.WriteLine($"да");
+//     }
+//     else
+//         Console.WriteLine($"нет");
+// }
+// else
+// {
+//     Console.WriteLine($"Вы ввели не верное значение");
+// }
