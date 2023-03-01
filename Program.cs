56964386cbba485ad3b669f11548a8df828ac25c
BOBAH1988 <79042044926@ya.ru>
2023-03-01 11:18:57 +0300

    finish dz1

--- Program.cs
+++ Program.cs
@@ -23,17 +23,30 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// Console.WriteLine("Введите 5-ти значное число: ");
-// int value = Convert.ToInt32(Console.ReadLine());
-// if(value > 9999 && value < 100000)
-// {
-//     int number = (value / 100) % 10;
-//     Console.WriteLine($"Ответ 1: {number}");
-// }
-// else
-// {
-//     Console.WriteLine($"Третей цифры нет!");
-// }
+Console.WriteLine("Введите 5-ти значное число: ");
+//int value = Convert.ToInt32(Console.ReadLine());
+int value = 12345;
+if(value > 9999 && value < 100000)
+{
+    // int number1 = (value / 10000);
+    // Console.WriteLine($"Ответ 1я цифра: {number1}");
+
+    // int number2 = (value / 1000) % 10;
+    // Console.WriteLine($"Ответ 2я цифра: {number2}");
+
+     int number3 = (value / 100) % 10;
+     Console.WriteLine($"Ответ 3я цифра: {number3}");
+
+    // int number4 = (value / 10) % 10;
+    // Console.WriteLine($"Ответ 4я цифра: {number4}");
+
+    // int number5 = (value % 10);
+    // Console.WriteLine($"Ответ 5я цифра: {number5}");
+}
+else
+{
+    Console.WriteLine($"Третей цифры нет!");
+}
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, 
